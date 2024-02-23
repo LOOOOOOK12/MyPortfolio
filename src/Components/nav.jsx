a48a02cd2b6940457e367b94df709ce6aa613ee2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-23 21:05:39 +0800

    update

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -4,7 +4,7 @@ function nav() {
     return (
         <div className='w-full'>
             <nav className='px-16 p-4 w-full top-0 z-10 fixed flex justify-between items-center text-center bg-slate-500'>
-                <h1>LOGO</h1>
+                <h1 className='text-3xl'>LOGO</h1>
                 <ul className='flex gap-8'>
                     <li>Home</li>
                     <li>About Me</li>

ab6fd17c230836b4c055cbee6f8c1941925ca8fe
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-28 17:27:55 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Nav from './Components/nav'
 import Hero from './Pages/Hero'
+import AboutMe from './Pages/AboutMe'
+import Projects from './Pages/Projects'
+import Certificates from './Pages/Certificates'
+import conta
 import './App.css'
+
 
 function App() {
   
@@ -9,6 +14,9 @@ function App() {
     <>
       <Nav/>
       <Hero/>
+      <AboutMe/>
+      <Projects/>
+      <Certificates/>
     </>
   )
 }

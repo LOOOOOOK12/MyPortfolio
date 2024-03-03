0a44a6e99a263bba17088dc73c2df9c77ae1157b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-03 16:13:16 +0800

    update

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import album from '../assets/Images/album.png'
 import { Element } from 'react-scroll'
 
 function Hero() {
     return (
-        <Element name='Hero' className='h-screen bg-slate-200 flex justify-center items-center'>
-            <h1>Home</h1>
+        <Element name='Hero' className='h-screen bg-slate-200 flex flex-col'>
+            <div className='h-[70vh] w-full flex flex-col gap-5 items-center justify-center bg-slate-400'>
+                <img src={album} alt="" className='h-40 rounded-full' />
+                <h1>loek</h1>
+            </div>
         </Element>
     )
 }

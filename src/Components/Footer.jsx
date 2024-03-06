e8415f71fb168aa71e0d1b9875ab14e5d908be06
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-06 18:52:47 +0800

    update

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='flex flex-col bg-slate-700 h-40'>
+            <div>
+                
+            </div>
+            <p>Made with love by Luke Symon Alamil</p>
+        </div>
+    )
+}
+
+export default Footer

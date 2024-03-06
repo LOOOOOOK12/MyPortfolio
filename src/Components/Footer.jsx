0a9a1bfa93e33958e8bce497a58ee6334aa37176
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-06 18:53:41 +0800

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='flex flex-col bg-slate-700 h-40'>
+        <div className='flex flex-col justify-center items-center text-slate-50 bg-slate-700 h-40'>
             <div>
                 
             </div>

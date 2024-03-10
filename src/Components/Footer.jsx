7f27678b8235bdfdd8a367c844ce03ca03763e25
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-10 23:52:41 +0800

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { links } from '../Constants/links'
+import Logos from './logos'
 
 function Footer() {
     return (
         <div className='flex flex-col justify-center items-center text-slate-50 bg-slate-700 h-40'>
-            <div>
-                
-            </div>
             <p>Made with love by Luke Symon Alamil</p>
+            <div className='flex gap-5'>
+                {links.map((l)=>(
+                    <Logos
+                        logo ={l.logo}
+                        alt={l.alt}
+                        links={l.link}
+                    />
+                ))}
+            </div>
         </div>
     )
 }

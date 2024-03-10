7384c8850bd96d043207f55c8a3c6b76554eb321
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-10 23:52:54 +0800

    update

--- src/Pages/ContactMe.jsx
+++ src/Pages/ContactMe.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ContactMe() {
     return (
         <div id='ContactMe' className='h-screen bg-gray-400 flex justify-center items-center'>
-            <h1>Contact</h1>
+            <h1>Contact Me</h1>
         </div>
     )
 }

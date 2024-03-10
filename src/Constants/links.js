32ba7741c1e4e6cf06a505807a989225238fddf6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-10 23:52:49 +0800

    update

---
+++ src/Constants/links.js
@@ -0,0 +1,17 @@
+export const links = [
+    {
+        logo: "woah",
+        alt: "Github",
+        link: "https://github.com/LOOOOOOK12",
+    },
+    {
+        logo: "Woah",
+        alt: "Github",
+        link: "https://github.com/LOOOOOOK12",
+    },
+    {
+        logo: "woah",
+        alt: "Github",
+        link: "https://github.com/LOOOOOOK12",
+    },
+]

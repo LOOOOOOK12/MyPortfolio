154b2d9070aedd4846f68714710c71e19dd64078
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-12 22:04:18 +0800

    update

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -6,7 +6,7 @@ function nav() {
         <div className='w-full'>
             <nav className='px-16 p-4 w-full top-0 z-10 fixed flex justify-between items-center text-center bg-slate-500'>
                 <Link to='Hero' smooth={true} duration={500}><h1 className='text-3xl cursor-default'>LOGO</h1></Link>
-                <ul className='flex gap-8'>
+                <ul className='flex gap-8 max-[780px]:hidden'>
                     <Link to='Hero' smooth={true} duration={500} className='cursor-default'>Home</Link>
                     <Link to='AboutMe' smooth={true} duration={500} className='cursor-default'>About Me</Link>
                     <Link to='Projects' smooth={true} duration={500} className='cursor-default'>Projects</Link>

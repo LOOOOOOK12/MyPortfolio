616a375c0ce78d7bda76e9f1a5483e7226ede4c8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-16 18:06:48 +0800

    update

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -6,8 +6,10 @@ function Hero() {
     return (
         <Element name='Hero' className='h-screen relative flex flex-col justify-center items-center'>
             <img src={album} className='absolute brightness-50 h-full w-full object-cover -z-10'/>
-            <img src={album} alt="" className='h-40 rounded-full' />
-            <h1>loek</h1>
+            <div className='flex flex-col items-center gap-5'>
+                <img src={album} alt="" className='h-40 rounded-full' />
+                <h1 className='text-gray-300 text-3xl'>Luke Symon A. Alamil</h1>
+            </div>
         </Element>
     )
 }

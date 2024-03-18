d63611292642548f7858e7006c3ec38deaf37708
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-18 17:14:36 +0800

    update

---
+++ src/Constants/certificates.js
@@ -0,0 +1,15 @@
+import SysAd from '../assets/certificates/SysAd.png'
+import JavaFoundation from '../assets/certificates/Java.png'
+
+export const certificates = [
+    {
+        certificate:SysAd,
+        title:"System Administration",
+        alt:"SAP Certificate",
+    },
+    {
+        certificate:JavaFoundation,
+        title:"Java",
+        alt:"Java Certificate",
+    },
+]

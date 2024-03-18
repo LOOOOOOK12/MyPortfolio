7319072a559a869d517e89f8952a55beb4e5becc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-18 17:14:40 +0800

    update

--- src/Pages/Certificates.jsx
+++ src/Pages/Certificates.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { certificates } from '../Constants/certificates'
 
-function Certificates() {
+function CertiContainer({certificate,alt,title}) {
     return (
-        <div id='Certificates' className='h-screen bg-slate-700 flex justify-center items-center'>
-            <h1>Certificate</h1>
+        <div className='flex flex-col items-center gap-2'>
+            <img className='h-64' src={certificate} alt={alt} />
+            <h1 className='text-2xl'>{title}</h1>
+        </div>
+    );
+}
+
+export default function Certificates() {
+    return (
+        <div id='Certificates' className='p-24 bg-slate-700 flex flex-col gap-16 justify-center items-center'>
+            <h1 className='text-5xl'>Certificates</h1>
+            <div className='grid md:grid-cols-3 gap-6'>
+                {certificates.map((m)=>(
+                    <CertiContainer
+                        certificate = {m.certificate}
+                        alt = {m.alt}
+                        title = {m.title}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
 
-export default Certificates

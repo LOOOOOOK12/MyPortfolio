08b5e0a9c116964b60047b2ab010999b7e318679
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-20 21:17:52 +0800

    update

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import album from '../assets/Images/album.png'
 import pfp from '../assets/Images/pfp.jpg'
-import { Element } from 'react-scroll'
 
 function Hero() {
     return (
-        <Element name='Hero' className='h-screen relative flex flex-col justify-center items-center'>
+        <div id='Hero' className='h-screen relative flex flex-col justify-center items-center'>
             <img src={album} className='absolute brightness-50 h-full w-full object-cover -z-10'/>
             <div className='flex flex-col items-center gap-5'>
                 <img src={pfp} alt="" className='h-52 rounded-full' />
                 <h1 className=' text-3xl'>Luke Symon A. Alamil</h1>
             </div>
-        </Element>
+        </div>
     )
 }
 

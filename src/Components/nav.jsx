4a91a58cb9bf3948965f50348abc5ab7d1e448d7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-20 21:17:42 +0800

    update

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { Link } from 'react-scroll';
 
 function nav() {
     return (
         <div className='w-full'>
             <nav className='px-16 p-4 w-full top-0 z-10 fixed flex justify-between items-center text-center'>
-                <Link to='Hero' smooth={true} duration={500}><h1 className='text-3xl cursor-default text-gray-300'>LOGO</h1></Link>
+                <a href='#Hero'><h1 className='text-3xl cursor-default text-gray-300'>LOGO</h1></a>
                 <ul className='flex gap-8 max-[780px]:hidden'>
-                    <Link to='Hero' smooth={true} duration={500} className='cursor-default'>Home</Link>
-                    <Link to='AboutMe' smooth={true} duration={500} className='cursor-default'>About Me</Link>
-                    <Link to='Projects' smooth={true} duration={500} className='cursor-default'>Projects</Link>
-                    <Link to='Certificates' smooth={true} duration={500} className='cursor-default'>Certificates</Link>
-                    <Link to='ContactMe' smooth={true} duration={500} className='cursor-default'>Contact Me</Link>
+                    <a href='#Hero' className='cursor-default'>Home</a>
+                    <a href='#AboutMe' className='cursor-default'>About Me</a>
+                    <a href='#Projects' className='cursor-default'>Projects</a>
+                    <a href='#Certificates' className='cursor-default'>Certificates</a>
+                    <a href='#ContactMe' className='cursor-default'>Contact Me</a>
                 </ul>
             </nav>
         </div>

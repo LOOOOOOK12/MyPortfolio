63facd5c04e4500cde77f47b32992f9d310904ea
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-21 22:56:30 +0800

    update

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
-import album from '../assets/Images/album.png'
 import pfp from '../assets/Images/pfp.jpg'
+import { heroSwiper } from '../Constants/heroSwiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay,EffectFade } from 'swiper/modules';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
+import 'swiper/css';
 
 function Hero() {
     return (
         <div id='Hero' className='h-screen relative flex flex-col justify-center items-center'>
-            <img src={album} className='absolute brightness-50 h-full w-full object-cover -z-10'/>
-            <div className='flex flex-col items-center gap-5'>
+            <Swiper
+                modules={[Autoplay,EffectFade]}
+                effect='fade'
+                autoplay = {{
+                disableOnInteraction:false,
+                delay: 5000
+                }}
+                loop
+                className='absolute w-full h-full'
+                >
+                {heroSwiper.map((img,idx)=>(
+                    <SwiperSlide key={idx} className='w-full h-full'>
+                        <img src={img.image} alt={img.alt} className='absolute brightness-[.33] h-full w-full object-cover -z-10'/>
+                    </SwiperSlide>  
+                ))}
+            </Swiper>
+            <div className='flex flex-col items-center gap-5 z-10'>
                 <img src={pfp} alt="" className='h-52 rounded-full' />
                 <h1 className=' text-3xl'>Luke Symon A. Alamil</h1>
             </div>

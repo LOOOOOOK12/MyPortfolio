5729cf2144bd62dfa7ade6c41e4da655719759c6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-21 22:56:26 +0800

    update

---
+++ src/Constants/heroSwiper.js
@@ -0,0 +1,23 @@
+import test1 from '../assets/Swipers/Hero-Swipers/test1.jpg'
+import test2 from '../assets/Swipers/Hero-Swipers/test2.jpg'
+import test3 from '../assets/Swipers/Hero-Swipers/test3.png'
+import test4 from '../assets/Images/album.png'
+
+export const heroSwiper = [
+    {
+        image: test1,
+        alt: "image1"
+    },
+    {
+        image: test2,
+        alt: "image2"
+    },
+    {
+        image: test3,
+        alt: "image3"
+    },
+    {
+        image: test4,
+        alt:"image4"
+    }
+]

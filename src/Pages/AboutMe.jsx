bef376a18edb2440218a14cb6c445ac5dd6d2493
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-27 17:32:35 +0800

    contents added

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import image from '../assets/Images/album.png'
 
 function AboutMe() {
     return (
-        <div id='AboutMe' className='h-screen bg-slate-800 flex justify-center items-center'>
-            <h1>About Me</h1>
+        <div id='AboutMe' className='h-screen p-40 gap-20 bg-slate-800 flex items-center'>
+            <img src={image} alt="" className='h-[30rem] w-[25rem]'/>
+            <div className='flex flex-col gap-8'>
+                <h1 className='text-4xl'>About Me</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
+                    iste praesentium minus nam velit, rem dicta, molestiae perferendis dolore illum dolores nemo veritatis. Repellat, deserunt.
+                </p>
+            </div>
         </div>
     )
 }

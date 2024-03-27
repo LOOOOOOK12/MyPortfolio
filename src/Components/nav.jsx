daabe175bc26c41fa2f0577d336a6792c79fd6cc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-27 17:32:03 +0800

    z-index

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function nav() {
     return (
-        <div className='w-full'>
+        <div className='w-full z-20'>
             <nav className='px-16 p-4 w-full top-0 z-10 fixed flex justify-between items-center text-center'>
                 <a href='#Hero'><h1 className='text-3xl cursor-default text-gray-300'>LOGO</h1></a>
                 <ul className='flex gap-8 max-[780px]:hidden'>

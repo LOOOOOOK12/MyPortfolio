55b97bcf0e8c17d89e59452c7580af234f620300
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-30 16:48:17 +0800

    fixed

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -6,10 +6,11 @@ function AboutMe() {
         <div id='AboutMe' className='h-screen p-40 gap-20 bg-slate-800 flex items-center'>
             <img src={image} alt="" className='h-[30rem] w-[25rem]'/>
             <div className='flex flex-col gap-8'>
-                <h1 className='text-4xl'>About Me</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
+                <h1 className='text-6xl'>About Me</h1>
+                <p className='text-3xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
                     iste praesentium minus nam velit, rem dicta, molestiae perferendis dolore illum dolores nemo veritatis. Repellat, deserunt.
                 </p>
+                <a href="">Download CV</a>
             </div>
         </div>
     )

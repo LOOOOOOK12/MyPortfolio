cbcce1a0072d01283cd1f3d87e815132888817e2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-09 16:10:15 +0800

    skills constant added

--- src/Constants/skills.js
+++ src/Constants/skills.js
@@ -1,3 +1,34 @@
+import js from '../assets/logos/progLogos/js.png'
+
 export const skills = [
-    
+    {
+        logo: js,
+        alt: "javascript",
+        title: "Javascript"
+    },
+    {
+        logo: js,
+        alt: "javascript",
+        title: "Javascript"
+    },
+    {
+        logo: js,
+        alt: "javascript",
+        title: "Javascript"
+    },
+    {
+        logo: js,
+        alt: "javascript",
+        title: "Javascript"
+    },
+    {
+        logo: js,
+        alt: "javascript",
+        title: "Javascript"
+    },
+    {
+        logo: js,
+        alt: "javascript",
+        title: "Javascript"
+    },
 ]

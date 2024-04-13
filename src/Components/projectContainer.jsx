e55f0a9a7f7b7a6ffba9038fa844c77ddf62606d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-13 16:43:11 +0800

    added project container

---
+++ src/Components/projectContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function projectContainer({image,alt,title,desc,language,github,link}) {
+    return (
+        <div className='flex flex-col md:flex-row gap-8 justify-center items-center max-w-xl'>
+            <img src={image} alt={alt} className='h-48 w-full rounded-md'/>
+            <div className='flex flex-col items-center gap-3 w-auto'>
+                <h1 className='text-3xl'>{title}</h1>
+                <p className='text-center'>{desc}</p>
+                <div className='flex gap-5'>
+                    {language.map((lang,idx) =>(
+                        <h2 key={idx}>{lang}</h2>
+                    ))}
+                </div>
+                <div className='flex gap-3'>
+                    <a href={github}>Github</a>
+                    <a href={link}>Demo</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default projectContainer

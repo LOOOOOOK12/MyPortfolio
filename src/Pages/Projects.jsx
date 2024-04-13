f516c0bd265797177909feb4af4cffd9e1cafd88
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-13 16:44:01 +0800

    mapped projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { projects } from '../Constants/projects'
+import ProjectContainer from '../Components/projectContainer'
 
 function Projects() {
     return (
-        <div id='Projects' className='h-screen bg-slate-900 flex justify-center items-center'>
-            <h1>Projects</h1>
+        <div id='Projects' className='p-16 bg-slate-900 flex flex-col gap-14 justify-center items-center'>
+            <h1 className='text-6xl'>Projects</h1>
+            <div className=' flex flex-col gap-28'>
+                {projects.map((p)=>(
+                    <ProjectContainer
+                        image = {p.image}
+                        alt = {p.alt}
+                        title = {p.title}
+                        desc = {p.desc}
+                        language = {p.language}
+                        github = {p.github}
+                        link = {p.link}
+                    />
+                ))}
+            </div>
         </div>
     )
 }

1ab376c4bbccd9cfee2ad36df27ab50d31edff73
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-13 16:43:23 +0800

    added projects constants

---
+++ src/Constants/projects.js
@@ -0,0 +1,31 @@
+import js from "../assets/logos/progLogos/js.png"
+
+export const projects = [
+    {
+        image: js,
+        alt:"alt",
+        title:"Booking Website",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod ",
+        github:"https://github.com/LOOOOOOK12/Booking-Website",
+        link:"https://booking-website-theta.vercel.app/Home",
+        language:["Javascript","Java","Python","Java","Python"]
+    },
+    {
+        image: js,
+        alt:"alt",
+        title:"Booking Website",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod ",
+        github:"https://github.com/LOOOOOOK12/Booking-Website",
+        link:"https://booking-website-theta.vercel.app/Home",
+        language:["javascript","java","python"]
+    },
+    {
+        image: js,
+        alt:"alt",
+        title:"Booking Website",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod ",
+        github:"https://github.com/LOOOOOOK12/Booking-Website",
+        link:"https://booking-website-theta.vercel.app/Home",
+        language:["javascript","java","python"]
+    }
+]

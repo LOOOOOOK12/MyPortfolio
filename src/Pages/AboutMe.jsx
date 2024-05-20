17c40384999ba7eb7d4568753417a39274072167
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-20 21:22:50 +0800

    gap changed and underline aadded in a tag

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -4,14 +4,14 @@ import sampleCv from '../assets/files/sample.pdf'
 
 function AboutMe() {
     return (
-        <div id='AboutMe' className='p-16 gap-20 flex flex-col items-center md:flex-row'>
+        <div id='AboutMe' className='p-16 gap-16 flex flex-col items-center md:flex-row'>
             <img src={image} alt="" className='h-[30rem] w-[25rem] rounded-md'/>
             <div className='flex flex-col gap-8 w-auto'>
                 <h1 className='text-6xl'>About Me</h1>
                 <p className='text-2xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
                     iste praesentium minus nam velit, rem dicta, molestiae perferendis dolore illum dolores nemo veritatis. Repellat, deserunt.
                 </p>
-                <a href={sampleCv} download>Download CV</a>
+                <a className="underline underline-offset-8" href={sampleCv} download >Download CV</a>
             </div>
         </div>
     )

28c1bead93943c4d49fd236bb93933dca01817ce
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-22 16:19:40 +0800

    text size changes grid added container size changes

--- src/Components/projectContainer.jsx
+++ src/Components/projectContainer.jsx
@@ -4,14 +4,14 @@ import Link from '../assets/logos/projectsLogo/link.png'
 
 function projectContainer({image,alt,title,desc,language,github,link}) {
     return (
-        <div className='flex flex-col md:flex-row gap-8 justify-center items-center md:w-[750px]'>
-            <img src={image} alt={alt} className='h-[300px] w-[500px] rounded-md'/>
+        <div className='flex flex-col md:flex-row gap-8 justify-center items-center md:w-[800px]'>
+            <img src={image} alt={alt} className='h-[300px] w-full md:w-[460px] rounded-md'/>
             <div className='flex flex-col gap-3 w-auto'>
-                <h1 className='text-3xl'>{title}</h1>
+                <h1 className='text-4xl'>{title}</h1>
                 <p className='text-left'>{desc}</p>
-                <div className='flex gap-5'>
-                    {language.map((lang,idx) =>(
-                        <h2 key={idx}>{lang}</h2>
+                <div className='grid grid-cols-3 gap-2'>
+                    {language.map((lang,idx) =>( 
+                        <p key={idx} className='bg-[#0c1727] text-center rounded-full px-2 border border-[#8a8a8a]'>{lang}</p> 
                     ))}
                 </div>
                 <div className='flex gap-3'>

1044bb6b9e84a25f3f6871a0f02b9dac68c662a2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-22 16:19:47 +0800

    added other websites

--- src/Constants/projects.js
+++ src/Constants/projects.js
@@ -1,31 +1,43 @@
 import js from "../assets/logos/progLogos/js.png"
-
+import personalWebsite from "../assets/logos/projectsLogo/personalWebsite.png"
+import appleWebsite from "../assets/logos/projectsLogo/Apples.png"
+import dreamSnare from "../assets/logos/projectsLogo/dreamSnare.png"
+import BookingWebsite from "../assets/logos/projectsLogo/bookingWebsite.png"
 export const projects = [
     {
-        image: js,
-        alt:"alt",
-        title:"Booking Website",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod ",
-        github:"https://github.com/LOOOOOOK12/Booking-Website",
-        link:"https://booking-website-theta.vercel.app/Home",
-        language:["Javascript","Java","Python","Java","Python"]
+        image: personalWebsite,
+        alt:"Personal Website",
+        title:"Personal Website",
+        desc: "Personal Website / getting to know more of Me!",
+        github:"https://github.com/LOOOOOOK12/Profile",
+        link:"looooook.netlify.app",
+        language:["Html","Css","Javascript","Swiper-Js"]
     },
     {
-        image: js,
-        alt:"alt",
-        title:"Booking Website",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod ",
-        github:"https://github.com/LOOOOOOK12/Booking-Website",
-        link:"https://booking-website-theta.vercel.app/Home",
-        language:["Javascript","Java","Python"]
+        image: appleWebsite,
+        alt:"Apple Website",
+        title:"Apple Website",
+        desc: "E-commerce Apple Website",
+        github:"https://github.com/LOOOOOOK12/Apple",
+        link:"https://mansanas-net.netlify.app",
+        language:["Html","Css","Javascript", "Swiper-Js"]
     },
     {
-        image: js,
+        image: dreamSnare,
+        alt:"alt",
+        title:"DreamSnare",
+        desc: "Dream Saver website where you can write your recent dreams!!! (SERN Stack)",
+        github:"https://github.com/LOOOOOOK12/DreamSnare",
+        link:"https://github.com/LOOOOOOK12/DreamSnare",
+        language:["ReactJs","SQL","NextJs","ExpressJs","Tailwind"]
+    },
+    {
+        image: BookingWebsite,
         alt:"alt",
         title:"Booking Website",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod adipiscing elit, sed do eiusmod ",
+        desc: "Simple Booking website using react js and tailwind",
         github:"https://github.com/LOOOOOOK12/Booking-Website",
-        link:"https://booking-website-theta.vercel.app/Home",
-        language:["Javascript","Java","Python"]
+        link:"https://booking-website-6x7h31u8k-looooook12s-projects.vercel.app",
+        language:["ReactJs","Router-Dom","Tailwind","SwiperJs"]
     }
 ]

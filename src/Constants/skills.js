b344470824d7739941e1ab3ddf7755891ee00da2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-22 15:21:03 +0800

    added more logos

--- src/Constants/skills.js
+++ src/Constants/skills.js
@@ -1,10 +1,24 @@
 import js from '../assets/logos/progLogos/js.png'
+import html from '../assets/logos/progLogos/html.png'
+import css from '../assets/logos/progLogos/css-3.png'
+import typeScript from '../assets/logos/progLogos/typescript.png'
+import react from '../assets/logos/progLogos/react.png'
+import node from '../assets/logos/progLogos/node.png'
+import bootstrap from '../assets/logos/progLogos/bootstrap.png'
+import tailwind from '../assets/logos/progLogos/tailwind.png'
+import java from '../assets/logos/progLogos/java.png'
+import Csharp from '../assets/logos/progLogos/c-sharp.png'
 
 export const skills = [
     {
-        logo: js,
-        alt: "javascript",
-        title: "Javascript"
+        logo: html,
+        alt: "Html",
+        title: "Html"
+    },
+    {
+        logo: css,
+        alt: "Css",
+        title: "Css"
     },
     {
         logo: js,
@@ -12,23 +26,38 @@ export const skills = [
         title: "Javascript"
     },
     {
-        logo: js,
+        logo: react,
+        alt: "React JS",
+        title: "React JS"
+    },
+    {
+        logo: typeScript,
         alt: "javascript",
         title: "Javascript"
     },
     {
-        logo: js,
-        alt: "javascript",
-        title: "Javascript"
+        logo: node,
+        alt: "Node JS",
+        title: "Node JS"
     },
     {
-        logo: js,
-        alt: "javascript",
-        title: "Javascript"
+        logo: bootstrap,
+        alt: "Bootstrap",
+        title: "Bootstrap"
     },
     {
-        logo: js,
-        alt: "javascript",
-        title: "Javascript"
+        logo: tailwind,
+        alt: "Tailwind Css",
+        title: "Tailwind Css"
+    },
+    {
+        logo: java,
+        alt: "Java",
+        title: "Java"
+    },
+    {
+        logo: Csharp,
+        alt: "C#",
+        title: "C#"
     },
 ]

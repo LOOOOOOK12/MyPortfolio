86546cb1fc057f992b0765984d964dbd25e10be7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-23 23:27:30 +0800

    padding changes

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function nav() {
     return (
         <div className='w-full z-20'>
-            <nav className='px-16 p-5 w-full top-0 z-50 fixed flex justify-between items-center text-center'>
+            <nav className='px-8 p-5 w-full top-0 z-50 fixed flex justify-between items-center text-center'>
                 <a href='#Hero'><h1 className='text-2xl cursor-default text-gray-300 underline underline-offset-8 decoration-blue-400 -rotate-6'>Loak☕</h1></a>
                 <ul className='flex gap-8 max-[780px]:hidden'>
                     <a href='#Hero' className='cursor-default hover:text-primary transition ease-in delay-75'>Home</a>

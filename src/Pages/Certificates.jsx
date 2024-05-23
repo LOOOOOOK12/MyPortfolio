ebaa5f38be3f5fa79144c1a297e0469281253b5a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-23 23:29:02 +0800

    image size changes and padding

--- src/Pages/Certificates.jsx
+++ src/Pages/Certificates.jsx
@@ -4,7 +4,7 @@ import { certificates } from '../Constants/certificates'
 function CertiContainer({certificate,alt,title}) {
     return (
         <div className='flex flex-col items-center gap-3'>
-            <img className='h-64' src={certificate} alt={alt} />
+            <img className='h-64 rounded-md' src={certificate} alt={alt} />
             <h1 className='text-2xl'>{title}</h1>
         </div>
     );
@@ -12,7 +12,7 @@ function CertiContainer({certificate,alt,title}) {
 
 export default function Certificates() {
     return (
-        <div id='Certificates' className='p-14 flex flex-col gap-16 justify-center items-center'>
+        <div id='Certificates' className='p-8 flex flex-col gap-16 justify-center items-center'>
             <h1 className='text-5xl'>Certificates</h1>
             <div className='grid md:grid-cols-3 gap-10'>
                 {certificates.map((m)=>(

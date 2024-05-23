ce13248e6210b8a6e0f6e0b8bfb848554c7826ae
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-23 23:28:01 +0800

    width of container image and p changed to h1 changes

--- src/Components/projectContainer.jsx
+++ src/Components/projectContainer.jsx
@@ -4,14 +4,14 @@ import Link from '../assets/logos/projectsLogo/link.png'
 
 function projectContainer({image,alt,title,desc,language,github,link}) {
     return (
-        <div className='flex flex-col md:flex-row gap-8 justify-center items-center md:w-[800px]'>
-            <img src={image} alt={alt} className='h-[300px] w-full md:w-[460px] rounded-md'/>
-            <div className='flex flex-col gap-3 w-auto'>
+        <div className='flex flex-col gap-8 w-full justify-center items-center md:flex-row md:max-w-3xl'>
+            <img src={image} alt={alt} className='h-[300px] w-full rounded-md md:w-[450px]'/>
+            <div className='flex flex-col gap-3 w-full'>
                 <h1 className='text-4xl'>{title}</h1>
                 <p className='text-left'>{desc}</p>
                 <div className='grid grid-cols-3 gap-2'>
                     {language.map((lang,idx) =>( 
-                        <p key={idx} className='bg-[#0c1727] text-center rounded-full px-2 border border-[#8a8a8a]'>{lang}</p> 
+                        <h1 key={idx} className='bg-[#0c1727] text-sm text-center px-2 rounded-full border border-[#8a8a8a]'>{lang}</h1> 
                     ))}
                 </div>
                 <div className='flex gap-3'>

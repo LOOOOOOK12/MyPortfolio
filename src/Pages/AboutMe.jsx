a3cbd3e4ea4efcbdbbce5e250b12056d38489bfe
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-25 18:39:59 +0800

    image size changes

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -4,9 +4,9 @@ import sampleCv from '../assets/files/sample.pdf'
 
 function AboutMe() {
     return (
-        <div id='AboutMe' className='p-8 gap-10 flex flex-col items-center md:flex-row'>
-            <img src={image} alt="proofule" className='h-[20rem] w-[25rem]  rounded-md'/>
-            <div className='flex flex-col gap-8 w-auto'>
+        <div id='AboutMe' className='p-8 gap-10 flex flex-col justify-center items-center w-full md:flex-row'>
+            <img src={image} alt="proofule" className='h-[25rem] w-[25rem] md:w-[750px] md:h-[500px] rounded-md'/>
+            <div className='flex flex-col gap-8'>
                 <h1 className='text-6xl'>About Me</h1>
                 <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
                     iste praesentium minus nam velit, rem dicta, molestiae perferendis dolore illum dolores nemo veritatis. Repellat, deserunt.

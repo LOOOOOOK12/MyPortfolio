c223432a45274dfdee69f61b579e718e1f5ed3b5
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-05 15:37:45 +0800

    container is added and border and bg color

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -4,14 +4,16 @@ import sampleCv from '../assets/files/sample.pdf'
 
 function AboutMe() {
     return (
-        <div id='AboutMe' className='p-8 gap-10 flex flex-col justify-center items-center w-full md:flex-row'>
-            <img src={image} alt="proofule" className='h-[25rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
-            <div className='flex flex-col gap-8'>
-                <h1 className='text-6xl'>About Me</h1>
-                <p className='text-xl leading-relaxed'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
-                    iste praesentium minus nam velit, rem dicta, molestiae perferendis dolore illum dolores nemo veritatis. Repellat, deserunt.
-                </p>
-                <a className="underline underline-offset-8 hover:text-primary transition ease-in delay-75 text-2xl" href={sampleCv} download >Download CV</a>
+        <div id='AboutMe' className='p-8'>
+            <div className='flex flex-col p-8 bg-[#101014] border-[1px] border-[#3f3f3f] rounded-md gap-10 justify-center items-center w-full md:flex-row'>
+                <img src={image} alt="proofule" className='h-[17rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
+                <div className='flex flex-col gap-8'>
+                    <h1 className='text-5xl md:text-6xl font-semibold'>About Me</h1>
+                    <p className='text-base md:text-xl leading-relaxed'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
+                        iste praesentium minus nam velit, rem dicta, molestiae perferendis dolore illum dolores nemo veritatis. Repellat, deserunt.
+                    </p>
+                    <a className="underline underline-offset-8 hover:text-primary transition ease-in delay-75 text-2xl" href={sampleCv} download >Download CV</a>
+                </div>
             </div>
         </div>
     )

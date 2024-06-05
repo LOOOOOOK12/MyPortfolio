6f8aabf778c2c98b536f33f7278839e067fcbd61
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-05 15:37:27 +0800

    bg color border are added and grid changes to flex-wrap in laguages

--- src/Components/projectContainer.jsx
+++ src/Components/projectContainer.jsx
@@ -4,14 +4,14 @@ import Link from '../assets/logos/projectsLogo/link.png'
 
 function projectContainer({image,alt,title,desc,language,github,link}) {
     return (
-        <div className='flex flex-col gap-8 w-full justify-center items-center md:flex-row md:max-w-3xl'>
+        <div className='flex flex-col gap-8 p-5 bg-[#101014] border-[1px] border-[#3f3f3f] rounded-md  w-full justify-center items-center md:flex-row md:max-w-3xl'>
             <img src={image} alt={alt} className='h-[300px] w-full rounded-md md:w-[450px]'/>
             <div className='flex flex-col gap-3 w-full'>
                 <h1 className='text-4xl'>{title}</h1>
                 <p className='text-left leading-relaxed'>{desc}</p>
-                <div className='grid grid-cols-3 gap-3'>
+                <div className='flex flex-wrap gap-2'>
                     {language.map((lang,idx) =>( 
-                        <h1 key={idx} className='bg-[#0c1727] text-sm text-center py-1 rounded-full border border-[#8a8a8a]'>{lang}</h1> 
+                        <h1 key={idx} className='bg-[#0c1727] text-sm text-center py-1 px-2 rounded-[8px] border border-[#8a8a8a]'>{lang}</h1> 
                     ))}
                 </div>
                 <div className='flex gap-3'>

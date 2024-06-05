af940d9e8cabfb87cb6c3382a14eb039edd77cfa
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-05 15:38:46 +0800

    circles are now added and responsiveness

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,11 +5,11 @@ import ProjectContainer from '../Components/projectContainer'
 function Projects() {
     return (
         <div id='Projects' className='p-8 flex flex-col gap-14 justify-center items-center relative'>
-            {/* <div className='absolute h-[300px] w-[250px] -z-10 top-[300px] left-[300px] rounded-full blur-3xl opacity-[0.5] bg-accent md:top-[300px] md:left-[500px]'></div> */}
-            <h1 className='text-6xl'>Projects</h1>
+            <div className='absolute overflow-hidden size-[200px] -z-10 top-0 right-0 rounded-full blur-3xl opacity-[0.5] bg-accent md:top-[300px]'></div>
+            <div className='absolute size-[200px] -z-10 top-[30rem] right-[10rem] md:top-[130px] md:left-[50px] rounded-full blur-3xl opacity-35 bg-accent'></div>
+            <div className='absolute overflow-hidden size-[200px] top-[50rem] right-0 md:left-[250px] -z-10 rounded-full blur-3xl opacity-35 bg-accent'></div>
+            <h1 className='text-6xl font-semibold'>Projects</h1>
             <div className='flex flex-col items-center justify-center gap-16 w-full'>
-                {/* <div className='absolute h-[300px] w-[250px] -z-10 -top-32 -right-20 rounded-full blur-3xl opacity-35 bg-accent'></div>
-                <div className='absolute h-[300px] w-[250px] -z-10 top-32 -left-14 rounded-full blur-3xl opacity-35 bg-accent'></div> */}
                 {projects.map((p)=>(
                     <ProjectContainer
                         image = {p.image}
@@ -22,6 +22,7 @@ function Projects() {
                     />
                 ))}
             </div>
+            
         </div>
     )
 }

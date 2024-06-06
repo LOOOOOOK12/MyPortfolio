b5afba7b72d7ac5a5e216c6db0488fa6bc29121c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-06 15:09:01 +0800

    added header and a tag

--- src/Pages/ContactMe.jsx
+++ src/Pages/ContactMe.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function ContactMe() {
     return (
-        <div id='ContactMe' className='h-screen flex justify-center items-center'>
-            <h1>Contact Me</h1>
+        <div id='ContactMe' className='h-screen p-8 flex flex-col gap-5 justify-center items-center text-center'>
+            <h1 className='text-3xl md:text-5xl font-semibold '>Need Something? Let's Talk!</h1>
+            <a href="mailto:lukealamil12@example.com" className='px-3 py-2 bg-accent hover:bg-[#52299A] duration-100 rounded-md '>Contact Me!</a>
         </div>
     )
 }

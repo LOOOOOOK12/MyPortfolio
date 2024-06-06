1160cecab1748de7b4f3f1c43fe7aa8d5063d6ab
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-06 15:09:20 +0800

    h1 changes

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -8,7 +8,7 @@ function Projects() {
             <div className='absolute overflow-hidden size-[200px] -z-10 top-0 right-0 rounded-full blur-3xl opacity-[0.5] bg-accent md:top-[300px]'></div>
             <div className='absolute size-[200px] -z-10 top-[30rem] right-[10rem] md:top-[130px] md:left-[50px] rounded-full blur-3xl opacity-35 bg-accent'></div>
             <div className='absolute overflow-hidden size-[200px] top-[50rem] right-0 md:left-[250px] -z-10 rounded-full blur-3xl opacity-35 bg-accent'></div>
-            <h1 className='text-6xl font-semibold'>Projects</h1>
+            <h1 className='text-5xl md:text-6xl font-semibold'>Projects</h1>
             <div className='flex flex-col items-center justify-center gap-16 w-full'>
                 {projects.map((p)=>(
                     <ProjectContainer

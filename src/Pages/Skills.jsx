20f9615d68ef0c852aa463820e8b1d807badd69e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-06 15:09:34 +0800

    h1 and h2 changes

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -18,8 +18,8 @@ function SkillsContainer({logo,alt,title}) {
 function Skills() {
     return (
         <div className='flex flex-col items-center justify-center p-8 gap-4'>
-            <h1 className=' text-5xl md:text-6xl font-semibold mb-8'>Technologies</h1>
-            <h2 className='text-3xl mb-8'>That I used are...</h2>
+            <h1 className='text-4xl md:text-6xl font-semibold mb-8'>Technologies</h1>
+            <h2 className='text-2xl md:text-3xl mb-8'>That I used are...</h2>
                 <Swiper
                     modules={[Autoplay,FreeMode]}
                     slidesPerView={5}

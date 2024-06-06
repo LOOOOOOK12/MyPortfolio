84f5d967e3a7fcb7d9d7d15fefbd8cf03c05d5b4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-06 15:08:37 +0800

    padding and bg change

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -5,7 +5,7 @@ import sampleCv from '../assets/files/sample.pdf'
 function AboutMe() {
     return (
         <div id='AboutMe' className='p-8'>
-            <div className='flex flex-col p-8 bg-[#101014] border-[1px] border-[#3f3f3f] rounded-md gap-10 justify-center items-center w-full md:flex-row'>
+            <div className='flex flex-col p-4 bg-[#131315] border-[1px] border-[#3f3f3f] rounded-md gap-10 justify-center items-center w-full md:flex-row'>
                 <img src={image} alt="proofule" className='h-[17rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
                 <div className='flex flex-col gap-8'>
                     <h1 className='text-5xl md:text-6xl font-semibold'>About Me</h1>

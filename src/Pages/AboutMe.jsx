f92bffe92ad3ceff7f71edb4e85b965acbd2ce8e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-13 18:33:56 +0800

    padding changes

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -6,8 +6,8 @@ function AboutMe() {
     return (
         <div id='AboutMe' className='p-8 bg-transparent'>
             <div className='flex flex-col p-4 bg-[#131315] border-[1px] border-[#3f3f3f] rounded-md gap-10 justify-center items-center w-full md:flex-row'>
-                <img src={image} alt="proofule" className='h-[17rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
-                <div className='flex flex-col gap-8'>
+                <img src={image} alt="proofule" className='h-[20rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
+                <div className='flex flex-col gap-3'>
                     <h1 className='text-5xl md:text-6xl font-semibold'>About Me</h1>
                     <p className='text-base md:text-xl leading-relaxed'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ducimus vel ipsa vitae temporibus 
                         iste praesentium minus nam velit, rem dicta, molestiae perferendis dolore illum dolores nemo veritatis. Repellat, deserunt.

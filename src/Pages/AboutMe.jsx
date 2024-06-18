319f6c30bb69f057c4743b082b6ed9281b58eb59
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-18 18:34:46 +0800

    pfp changes

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import image from '../assets/Images/album.png'
 import sampleCv from '../assets/files/sample.pdf'
+import pfp from '../assets/Images/pfp.jpg'
 
 function AboutMe() {
     return (
         <div id='AboutMe' className='p-8 bg-transparent'>
             <div className='flex flex-col p-4 bg-[#121219] border-[1px] border-[#3f3f3f] rounded-md gap-10 justify-center items-center w-full md:flex-row'>
-                <img src={image} alt="proofule" className='h-[20rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
+                <img src={pfp} alt="proofule" className='h-[20rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
                 <div className='flex flex-col gap-3'>
                     <h1 className='text-3xl md:text-5xl font-semibold'>About Me</h1>
                     <p className='text-base leading-8 md:text-xl md:leading-8 text-justify'>I'm Luke Symon A. Alamil, 21 years old and currently a student and taking Bachelor of Science in Information Technology in STI College legazpi.

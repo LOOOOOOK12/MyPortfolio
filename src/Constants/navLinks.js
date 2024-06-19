f5cc33401dee4c398854986bbb6efccde41084f6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-19 17:20:36 +0800

    nav links added

---
+++ src/Constants/navLinks.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        link: "Home",
+        href: "#Hero",
+    },
+    {
+        link: "About Me",
+        href: "#AboutMe",
+    },
+    {
+        link: "Projects",
+        href: "#Projects",
+    },
+    {
+        link: "Certificates",
+        href: "#Certificates",
+    },
+    {
+        link: "Contact Me",
+        href: "#ContactMe",
+    },
+]

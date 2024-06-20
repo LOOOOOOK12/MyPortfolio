581ed4ee69cee1c00f52270385009cfd0bc9cdd2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-20 17:57:21 +0800

    border color and bg color changes

--- src/Components/projectContainer.jsx
+++ src/Components/projectContainer.jsx
@@ -4,14 +4,14 @@ import Link from '../assets/logos/projectsLogo/link.png'
 function projectContainer({image,alt,title,desc,language,github,link}) {
 
     return (
-        <div className='flex flex-col gap-8 p-4 bg-[#121219] border-[1px] border-[#3f3f3f] rounded-md  w-full justify-center items-center md:flex-row md:max-w-3xl'>
+        <div className='flex flex-col gap-8 p-4 bg-[#0e0e1a] border-[1px] border-[#4d5975] rounded-md  w-full justify-center items-center md:flex-row md:max-w-3xl'>
             <img src={image} alt={alt} className='h-[270px] w-full rounded-md md:w-[450px]'/>
             <div className='flex flex-col gap-3 w-full'>
                 <h1 className='text-4xl'>{title}</h1>
                 <p className='text-left leading-relaxed'>{desc}</p>
                 <div className='flex flex-wrap gap-2'>
                     {language.map((lang,idx) =>( 
-                        <h1 key={idx} className='bg-[#0c1727] text-sm text-center py-1 px-2 rounded-[8px] border border-[#8a8a8a]'>{lang}</h1> 
+                        <h1 key={idx} className='bg-[#0e0e1a] text-sm text-center py-1 px-2 rounded-[8px] border border-[#4d5975]'>{lang}</h1> 
                     ))}
                 </div>
                 <div className='flex gap-3'>

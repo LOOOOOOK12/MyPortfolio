f39e45b6526f122698de78bf17c37cd5d4a70b17
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-24 18:26:52 +0800

    fixes in button

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -18,6 +18,7 @@ function Nav() {
                         <a href={l.href} className='hover:text-primary  transition ease-in delay-75 text-base font-semibold'>{l.link}</a>
                     ))}
                 </ul>
+                {/* burger */}
                 <div className='min-[770px]:hidden'>
                     <div className='flex flex-col gap-2' onClick={toggleHamburger}>
                         <span className='h-[.2rem] w-[2rem] border border-primary rounded-xl bg-primary animate-pulse'></span>
@@ -25,10 +26,10 @@ function Nav() {
                         <span className='h-[.2rem] w-[2rem] border border-primary rounded-xl bg-primary animate-pulse'></span>
                     </div>
                     {hamOpen && (
-                        <div className='absolute top-0 right-0 h-[31rem] flex flex-col gap-10 p-10 z-10 bg-secondary'>
-                            <div className='flex flex-col items-end' onClick={toggleHamburger}>
-                                <span className='h-[.2rem] w-[2rem] -rotate-45 border border-primary rounded-xl bg-primary animate-pulse'></span>
-                                <span className='h-[.2rem] w-[2rem] -rotate-45 border border-primary rounded-xl bg-primary animate-pulse'></span>
+                        <div className='absolute top-0 right-0 h-[42rem] flex flex-col gap-10 p-10 z-10 bg-secondary'>
+                            <div className='flex flex-col items-end relative' onClick={toggleHamburger}>
+                                <span className='absolute h-[.2rem] w-[2rem] rotate-45 border border-primary rounded-xl bg-primary animate-pulse'></span>
+                                <span className=' absolute h-[.2rem] w-[2rem] -rotate-45 border border-primary rounded-xl bg-primary animate-pulse'></span>
                             </div>
                             <div className='flex flex-col gap-6'>
                                 {navLinks.map((l) => (

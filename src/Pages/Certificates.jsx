3d57309aeb10968ce623a22c68e943e0dfdc1577
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-25 17:39:39 +0800

    added framer motions

--- src/Pages/Certificates.jsx
+++ src/Pages/Certificates.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { certificates } from '../Constants/certificates'
+import { motion } from 'framer-motion';
 
 function CertiContainer({certificate,alt,title}) {
     return (
@@ -13,14 +14,27 @@ function CertiContainer({certificate,alt,title}) {
 export default function Certificates() {
     return (
         <div id='Certificates' className='p-8 flex flex-col gap-16 justify-center items-center bg-transparent'>
-            <h1 className='text-5xl md:text-6xl font-semibold'>Certificates</h1>
+            <motion.h1
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1 ,y: 0 }}
+                viewport={{ once: true }}
+                transition={{duration:1 }}
+                className='text-5xl md:text-6xl font-semibold'>Certificates</motion.h1>
             <div className='grid md:grid-cols-3 gap-10'>
-                {certificates.map((m)=>(
-                    <CertiContainer
+                {certificates.map((m,idx)=>(
+                    <motion.div
+                        initial={{opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 1, delay: 0.5 + idx * 0.5 }}
+                        key={idx}
+                        className='h-full w-full'>
+                        <CertiContainer
                         certificate = {m.certificate}
                         alt = {m.alt}
                         title = {m.title}
-                    />
+                        />
+                    </motion.div>
                 ))}
             </div>
         </div>

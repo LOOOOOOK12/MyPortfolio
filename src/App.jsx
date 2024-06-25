f50b4dda2a5112a025b1bf30d50a257170af6502
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-25 17:37:48 +0800

    circles moved

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,6 @@ function App() {
   
   return (
     <div className='relative overflow-hidden'>
-      {/* header */}
-      <div className='absolute overflow-hidden size-[18rem] -z-10 top-[12rem] left-[40rem] right-0 bottom-0 rounded-full blur-3xl opacity-[0.3] bg-accent'></div>
-      <div className='absolute overflow-hidden size-[10rem] -z-10 top-[22rem] left-[20rem] rounded-full blur-3xl opacity-[0.3] bg-accent'></div>
-      {/* technoogies */}
-      <div className='absolute overflow-hidden size-[15rem] -z-10 top-[68rem] right-[9rem] rounded-full blur-3xl opacity-[0.3] bg-accent'></div>
-      <div className='absolute overflow-hidden size-[15rem] -z-10 top-[97rem] left-[7rem] rounded-full blur-3xl opacity-[0.4] bg-accent' ></div>
-      {/* sa may contacts */}
-      <div className='absolute overflow-hidden size-[12rem] -z-10 md:top-[233rem] md:left-[13rem] rounded-full blur-3xl opacity-[0.5] bg-accent'></div>
-      <div className='absolute overflow-hidden size-[20rem] -z-10 md:top-[238rem] md:left-[40rem] rounded-full blur-3xl opacity-[0.3] bg-accent'></div>
-      <div className='absolute overflow-hidden size-[16rem] -z-10 md:top-[252rem] md:left-[13rem] rounded-full blur-3xl opacity-[0.2] bg-accent'></div>
       <Nav/>
       <Hero/>
       <AboutMe/>

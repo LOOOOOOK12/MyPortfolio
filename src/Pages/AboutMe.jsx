8a4257dccc353c03dfa6fb0a83d5138568a7330a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-25 17:39:29 +0800

    added framer motions

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import sampleCv from '../assets/files/sample.pdf'
 import pfp from '../assets/Images/pfp.jpg'
+import { motion } from 'framer-motion'
 
 function AboutMe() {
     return (
         <div id='AboutMe' className='p-8 bg'>
-            <div className='flex flex-col p-4 bg-[#0e0e1a] border-[1px] border-[#4d5975] rounded-md gap-10 justify-center items-center w-full md:flex-row'>
+            <div className='flex flex-col p-4 bg-[#0e0e1a] border-[1px] border-[#4d5975] rounded-md gap-10 justify-center items-center max-w-[90rem] md:flex-row'>
                 <img src={pfp} alt="proofule" className='h-[20rem] w-[25rem] md:w-[750px] md:h-[400px] rounded-md'/>
-                <div className='flex flex-col gap-3'>
-                    <h1 className='text-3xl md:text-5xl font-semibold'>About Me</h1>
-                    <p className='text-base leading-8 md:text-xl md:leading-8 text-justify'>I'm Luke Symon A. Alamil, 21 years old and currently a student and taking Bachelor of Science in Information Technology in STI College legazpi.
+                <div
+                    className='flex flex-col gap-3'>
+                    <motion.h1
+                        initial={{ opacity: 0, y:-100 }}
+                        whileInView={{ opacity: 1, y:0 }}
+                        transition={{ duration: 1 }}
+                        viewport={{ once:true }}
+                        className='text-3xl md:text-5xl font-semibold'>About Me</motion.h1>
+                    <motion.p
+                        initial={{ opacity: 0, x:100 }}
+                        whileInView={{ opacity: 1, x:0 }}
+                        transition={{ duration: 1 }}
+                        viewport={{ once:true }}
+                        className='text-base leading-8 md:text-xl md:leading-8 text-justify'>I'm Luke Symon A. Alamil, 21 years old and currently a student and taking Bachelor of Science in Information Technology in STI College legazpi.
                         I love arts and design which led me to study Web development and apply my skills designing and making Websites.  
-                    </p>
-                    <a className="underline underline-offset-8 hover:text-primary transition ease-in delay-75 text-2xl" href={sampleCv} download >Download CV</a>
+                    </motion.p>
+                    <motion.a
+                        initial={{ opacity: 0, x:100 }}
+                        whileInView={{ opacity: 1, x:0 }}
+                        transition={{ duration: 1 }}
+                        viewport={{ once:true }}
+                        className="underline underline-offset-8 hover:text-primary transition ease-in delay-75 text-2xl" href={sampleCv} download >Download CV</motion.a>
                 </div>
             </div>
         </div>

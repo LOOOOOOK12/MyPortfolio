abb1bf039ea05c7eb698432872e4865e4c7dbdc7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-25 17:40:19 +0800

    motions added

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import { projects } from '../Constants/projects'
 import ProjectContainer from '../Components/projectContainer'
+import { motion } from 'framer-motion'
 
 function Projects() {
     return (
         <div id='Projects' className='p-8 flex flex-col overflow-hidden gap-14 justify-center items-center relative bg-transparent'>
-            <h1 className='text-5xl md:text-6xl font-semibold'>Projects</h1>
+            <motion.h1
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1 ,y: 0 }}
+                viewport={{ once: true }}
+                transition={{duration:1 }}
+                className='text-5xl md:text-6xl font-semibold'>Projects</motion.h1>
             <div className='flex flex-col items-center justify-center gap-16 w-full'>
-                {projects.map((p)=>(
-                    <ProjectContainer
-                        image = {p.image}
-                        alt = {p.alt}
-                        title = {p.title}
-                        desc = {p.desc}
-                        language = {p.language}
-                        github = {p.github}
-                        link = {p.link}
-                    />
+                {projects.map((p,idx)=>(
+                    <motion.div
+                        initial={{opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 1, delay: 0.5 + idx * 0.05 }}
+                        key={idx}>
+                            <ProjectContainer
+                            image = {p.image}
+                            alt = {p.alt}
+                            title = {p.title}
+                            desc = {p.desc}
+                            language = {p.language}
+                            github = {p.github}
+                            link = {p.link}
+                        />
+                    </motion.div>
                 ))}
             </div>
             

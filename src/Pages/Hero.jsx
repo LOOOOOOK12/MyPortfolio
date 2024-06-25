a98f5f709a3eccbd97fdae70cde8f9b46bc611c3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-25 17:40:09 +0800

    circes added, added framer motions

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -11,12 +11,14 @@ import 'swiper/css';
 function Hero() {
     return (
         <div id='Hero' className='relative h-screen overflow-hidden flex flex-col justify-center items-center text-center'>
+            <div className='absolute overflow-hidden size-[10rem] -z-10 top-[5rem] right-[3rem] rounded-full blur-3xl opacity-[0.3] bg-[#4991f6]'></div>
+            <div className='absolute overflow-hidden size-[10rem] -z-10 bottom-[7rem] left-[5rem] rounded-full blur-3xl opacity-[0.3] bg-[#4991f6]'></div>
             <Marquee
                 direction='left'
                 autoFill = {true}
                 gradient = {true}
                 gradientColor='#050d18'
-                style={{position:'absolute', width:"100%", height:"100%", top:0, left:0, opacity:0.6, zIndex:5}}
+                style={{position:'absolute', width:"100%", height:"100%", top:0, left:0, opacity:0.4, zIndex:5}}
                //className="absolute w-full h-full top-0 left-0 opacity-45"  
             >
                 {skills.map((skill)=>(
@@ -26,7 +28,7 @@ function Hero() {
             <motion.div
                 initial={{opacity: 0, y: -100 }}
                 animate={{opacity: 1, y: 0 }}
-                transition={{ duration: 1, type:"spring", delay: .1 }}
+                transition={{ duration: 3, type:"spring", delay: .4 }}
                 className='flex flex-col items-center gap-5 z-10'>
                 <img src={pfp} alt="me" className='h-40 md:h-52 rounded-full' />
                 <h1 className='text-2xl font-semibold md:text-5xl'>Luke Symon A. Alamil</h1>

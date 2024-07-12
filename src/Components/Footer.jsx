db64c04d07da19fd12d687942266d8a424e1f23e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-12 15:00:47 +0800

    added icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { links } from '../Constants/links'
-import Logos from './logos'
+import React from 'react';
+import { links } from '../Constants/links';
 
 function Footer() {
     return (
         <div className='flex flex-col gap-5 justify-center items-center text-slate-50 h-40'>
             <p>Made with love by 💙 Luke Symon Alamil</p>
-            <p>Still in development~</p>
+            <div className='flex flex-row gap-3'>
+                {links.map((link, idx) => (
+                    <a key={idx} href={link.link} target='_blank' rel='noopener noreferrer'>
+                        {link.type === 'icon' ? (
+                            <link.icon size={35} />
+                        ) : (
+                            <img src={link.logo} alt={link.alt} />
+                        )}
+                    </a>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
 
-export default Footer
+export default Footer;

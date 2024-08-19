081409e59e0072117ddff4d5431421b56efa3cad
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-08-19 13:18:20 +0800

    💬 change in sentence

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { links } from '../Constants/links';
 function Footer() {
     return (
         <div className='flex flex-col gap-5 justify-center items-center text-slate-50 h-40'>
-            <p>Made with love by 💙 Luke Symon Alamil</p>
+            <p>Made with 💙 by Luke Symon Alamil</p>
             <div className='flex flex-row gap-3'>
                 {links.map((link, idx) => (
                     <a key={idx} href={link.link} target='_blank' rel='noopener noreferrer'>
